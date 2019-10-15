6b264654825936a821fd5511c5287a0270f78163
jcamilovelandiab <jcamilovelandiab@gmail.com>
2019-10-15 18:02:24 -0500

    math services done

---
+++ src/main/java/edu/escuelaing/arem/services/MathServices.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.arem.services;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan Camilo Velandia Botello
+ */
+public class MathServices {
+    
+    public static Integer square(Integer i){
+        return i*i;
+    }
+}
